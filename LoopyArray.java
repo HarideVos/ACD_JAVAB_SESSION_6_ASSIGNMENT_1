fdd16d97617a572927de4fb01022cb4c6c92a69e
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-15 07:45:01 -0400

    Add files via upload

---
+++ LoopyArray.java
@@ -0,0 +1,16 @@
+package sixpointone;
+
+public class LoopyArray {
+
+	public static void main(String[] args) {
+		int arr[] = new int[10];
+		for(int i=1; i<=arr.length; i++) {
+			if(i%2 == 0) {
+				System.out.print(" " + i);
+				
+			}
+		}
+
+	}
+
+}
